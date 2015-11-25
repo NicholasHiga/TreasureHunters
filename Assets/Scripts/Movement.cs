46ac94fbc8ce83dc1a9e7432a56fa3c2eeb39bf7
Nicholas Higa <nicholas_higa@hotmail.com>
2015-11-25 16:50:25 -0500

    Reorganizing + Simple Movement

---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Movement : MonoBehaviour
+{
+	public float horizontalSpeed;
+	public float verticalSpeed;
+	
+	void FixedUpdate()
+	{
+		if (Input.GetKey(KeyCode.A))
+			gameObject.transform.position += new Vector3(-horizontalSpeed * Time.fixedDeltaTime, 0, 0);
+
+		if (Input.GetKey(KeyCode.D))
+			gameObject.transform.position += new Vector3(horizontalSpeed * Time.fixedDeltaTime, 0, 0);
+
+		if (Input.GetKey(KeyCode.W))
+			gameObject.transform.position += new Vector3(0, verticalSpeed * Time.fixedDeltaTime, 0);
+
+		if (Input.GetKey(KeyCode.S))
+			gameObject.transform.position += new Vector3(0, -verticalSpeed * Time.fixedDeltaTime, 0);
+	}
+}
