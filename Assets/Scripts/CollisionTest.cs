d5b00df7aeb4ebe6d9934d578415b7e118b2bcf5
stephenlozada <stephenlozada@hotmail.com>
2015-12-05 18:02:05 -0500

    Test
    test

---
+++ Assets/Scripts/CollisionTest.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CollisionTest : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        if (col.gameObject.name == "Dug(Clone)")
+        {
+            Debug.Log("HIT");
+            Destroy(col.gameObject);
+        }
+    }
+}
---
+++ Assets/Scripts/Dig.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class Dig : MonoBehaviour {
+
+    float time = 0;
+    public static Image chargeBar;
+    public Transform digArea;
+    public AudioClip drillSound;
+
+	// Use this for initialization
+	void Start () {
+        chargeBar = GameObject.Find("Image").GetComponent<Image>();
+        chargeBar.fillAmount = 0;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        time += Time.deltaTime;
+        if (time >= 0.2f)
+        {
+           // GetComponent<AudioSource>().PlayOneShot(drillSound, 0.3f);
+            chargeBar.fillAmount += 0.01f;
+            //time = 0;           
+        }
+        if (chargeBar.fillAmount == 1.0f)
+        {
+            Destroy(gameObject);
+            Instantiate(digArea, transform.position, transform.rotation);
+        }
+	}
+}
