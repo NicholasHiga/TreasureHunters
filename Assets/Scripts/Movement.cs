4429d844a8b8dde8e038b07a45d4230d27b23683
stephenlozada <stephenlozada@hotmail.com>
2015-12-05 18:13:56 -0500

    fixed movement

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -3,21 +3,31 @@ using System.Collections;
 
 public class Movement : MonoBehaviour
 {
-	public float horizontalSpeed;
-	public float verticalSpeed;
-	
-	void FixedUpdate()
-	{
-		if (Input.GetKey(KeyCode.A))
-			gameObject.transform.position += new Vector3(-horizontalSpeed * Time.fixedDeltaTime, 0, 0);
+    public float horizontalSpeed;
+    public float verticalSpeed;
+    public Transform digMachine;
+    public float digDelay = 3.0f;
+    float cooldownTimer = 0f;
 
-		if (Input.GetKey(KeyCode.D))
-			gameObject.transform.position += new Vector3(horizontalSpeed * Time.fixedDeltaTime, 0, 0);
+    void FixedUpdate()
+    {
+        cooldownTimer -= Time.deltaTime;
 
-		if (Input.GetKey(KeyCode.W))
-			gameObject.transform.position += new Vector3(0, verticalSpeed * Time.fixedDeltaTime, 0);
+        if (Input.GetKey(KeyCode.A))
+            gameObject.transform.position += new Vector3(-horizontalSpeed * Time.fixedDeltaTime, 0, 0);
 
-		if (Input.GetKey(KeyCode.S))
-			gameObject.transform.position += new Vector3(0, -verticalSpeed * Time.fixedDeltaTime, 0);
-	}
+        if (Input.GetKey(KeyCode.D))
+            gameObject.transform.position += new Vector3(horizontalSpeed * Time.fixedDeltaTime, 0, 0);
+
+        if (Input.GetKey(KeyCode.W))
+            gameObject.transform.position += new Vector3(0, verticalSpeed * Time.fixedDeltaTime, 0);
+
+        if (Input.GetKey(KeyCode.S))
+            gameObject.transform.position += new Vector3(0, -verticalSpeed * Time.fixedDeltaTime, 0);
+        if (Input.GetKey(KeyCode.Space) && cooldownTimer <= 0)
+        {
+            cooldownTimer = digDelay;
+            Instantiate(digMachine, transform.position, transform.rotation);
+        }
+    }
 }
