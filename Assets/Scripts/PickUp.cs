f64e87881fbbd0fdcfdfbd5c90615333e8f8d0f0
Chemel Lemarr Bennett <chemelbennett@gmail.com>
2015-12-15 20:01:28 -0500

    fixed health pick up
    need to make is so if at 100% health u cant over heal

---
+++ Assets/Scripts/PickUp.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PickUp : MonoBehaviour {
+	float  currentHealth = 0;
+	float maxHealth = 100;
+	float calculatedHealth;
+	public GameObject HealthBar;
+
+
+	void OnTriggerEnter2D(Collider2D col)
+	{
+		if (col.gameObject.tag=="Chicken")
+		{
+
+			currentHealth += 100;
+			calculatedHealth = currentHealth / maxHealth;
+			setHealthBar(calculatedHealth);
+		}
+	}
+	public void setHealthBar(float myHealth)
+	{
+		HealthBar.transform.localScale = new Vector3 (myHealth, HealthBar.transform.localScale.y, HealthBar.transform.localScale.z);
+	}
+}
