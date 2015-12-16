87c4bd126f08cbab038922f7b8a11d9f8d2ff527
stephenlozada <stephenlozada@hotmail.com>
2015-12-16 07:40:45 -0500

    random digging

--- Assets/Scripts/Dig.cs
+++ Assets/Scripts/Dig.cs
@@ -2,29 +2,33 @@
 using System.Collections;
 using UnityEngine.UI;
 
-public class Dig : MonoBehaviour {
+public class Dig : MonoBehaviour
+{
 
     float time = 0;
     public Image chargeBar;
     public Transform digArea;
     public Transform purplePowerUp;
     public Transform redPowerUp;
+    public Transform treasure;
     public AudioClip drillSound;
     private int number;
 
-	// Use this for initialization
-	void Start () {
-		chargeBar = gameObject.transform.FindChild("Canvas").FindChild("Image").GetComponent<Image>();
-		chargeBar.fillAmount = 0;
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		time += Time.deltaTime;
-        number = Random.Range(1, 10);
+    // Use this for initialization
+    void Start()
+    {
+        chargeBar = gameObject.transform.FindChild("Canvas").FindChild("Image").GetComponent<Image>();
+        chargeBar.fillAmount = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        time += Time.deltaTime;
+        number = Random.Range(1, 4);
         if (time >= 0.2f)
         {
-           // GetComponent<AudioSource>().PlayOneShot(drillSound, 0.3f);
+            // GetComponent<AudioSource>().PlayOneShot(drillSound, 0.3f);
             chargeBar.fillAmount += 0.01f;
             //time = 0;           
         }
@@ -40,6 +44,10 @@ public class Dig : MonoBehaviour {
             {
                 Instantiate(redPowerUp, transform.position, transform.rotation);
             }
+            if (number == 3)
+            {
+                Instantiate(treasure, transform.position, transform.rotation);
+            }
         }
-	}
+    }
 }
